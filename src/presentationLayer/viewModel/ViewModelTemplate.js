5923e6424ad94e260a59697cf49e0e99de41afe1
youngsupchoi <sup1214@naver.com>
2023-09-05 17:39:53 +0900

    Docs: template문서 추가

---
+++ src/presentationLayer/viewModel/ViewModelTemplate.js
@@ -0,0 +1,43 @@
+import {useState} from 'react';
+
+// 1. 필요한 뷰 스테이트 가져오기 (작명규칙: use + view이름 + State)
+//import {useCustomViewState} from './viewState';
+
+// 2. 데이터 소스 또는 API 가져오기
+//import {fetchExampleData} from '../dataLayer/dataSource';
+
+// 3. 뷰 모델 hook 이름 변경하기 (작명규칙: use + view이름 + ViewModel)
+export const useMainViewModel = () => {
+  // 뷰 스테이트의 상태와 액션 가져오기
+  const {state, actions} = useMainViewState();
+
+  // 4. 뷰 모델에서만 사용되는 상태 선언하기 (예: products)
+  //const [exampleData, setExampleData] = useState([]);
+
+  // 5. 필요한 로직 작성하기 (예: 데이터 검색)
+  const fetchExample = async () => {
+    actions.setExampleState(true);
+    try {
+      const [data, error] = await fetchExampleData();
+      if (error) {
+        throw new Error(error);
+      }
+      setExampleData(data);
+    } catch (e) {
+      actions.SetExampleErrorMessage(e.title);
+      actions.setExampleErrorTitle(e.message);
+    }
+    actions.setExampleState(false);
+  };
+
+  return {
+    state: {
+      ...state,
+      exampleData: exampleData,
+    },
+    actions: {
+      ...actions,
+      fetchExample,
+    },
+  };
+};
---
+++ src/presentationLayer/viewState/ViewSteateTemplate.js
@@ -0,0 +1,37 @@
+import {createContext, useContext, useState, useRef} from 'react';
+
+// 1. Context 이름 변경하기, 작명규치 - view이름 + Context
+const CustomViewContext = createContext();
+
+// 2. Hook 이름 변경하기, 작명 규칙- use + view이름 + State
+export const useCustomViewState = () => {
+  const context = useContext(CustomViewContext);
+  if (!context) {
+    throw new Error(
+      'useCustomViewState must be used within a CustomViewStateProvider',
+    );
+  }
+  return context;
+};
+//provider작명규칙 - view이름 + Provider
+export const CustomViewStateProvider = ({children}) => {
+  // 3. 필요한 상태 추가하기
+  const [exampleState, setExampleState] = useState(null);
+  // ... 다른 상태들
+
+  const state = {
+    exampleState,
+    // ... 다른 상태들
+  };
+
+  const actions = {
+    setExampleState,
+    // ... 다른 상태 설정 함수들
+  };
+
+  return (
+    <CustomViewContext.Provider value={{state, actions}}>
+      {children}
+    </CustomViewContext.Provider>
+  );
+};
