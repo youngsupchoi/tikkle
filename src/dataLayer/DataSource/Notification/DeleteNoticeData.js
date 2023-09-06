f7e9bc796df417a6ca725c2452944cc965bc7d77
sjsjmine129 <sjsjmine129@gmail.com>
2023-09-06 14:54:33 +0900

    Fix/ return token

--- src/dataLayer/DataSource/Notification/DeleteNoticeData.js
+++ src/dataLayer/DataSource/Notification/DeleteNoticeData.js
@@ -67,13 +67,6 @@ export async function deleteNoticeData(notificationId) {
       response.data.returnToken,
       authorization,
     );
-    if (!response_setToken) {
-      return {
-        DScode: 3,
-        DSdata: null,
-        DSmessage: '로그인이 만료 되었어요. 다시 로그인해주세요.',
-      };
-    }
   }
 
   //------ return response ------------------------------------------------//
--- src/dataLayer/DataSource/Product/GetProductListData.js
+++ src/dataLayer/DataSource/Product/GetProductListData.js
@@ -78,13 +78,6 @@ export async function getProductListData(
       response.data.returnToken,
       authorization,
     );
-    if (!response_setToken) {
-      return {
-        DScode: 3,
-        DSdata: null,
-        DSmessage: '로그인이 만료 되었어요. 다시 로그인해주세요.',
-      };
-    }
   }
 
   //------ return response ------------------------------------------------//
