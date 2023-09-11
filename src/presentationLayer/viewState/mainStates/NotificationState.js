ba61284c0b531e50b958ca8c1c5d773fd998818d
sjsjmine129 <sjsjmine129@gmail.com>
2023-09-11 13:55:24 +0900

    Fix/ notification screen decouple, axios->DS

---
+++ src/presentationLayer/viewState/mainStates/NotificationState.js
@@ -0,0 +1,48 @@
+import {createContext, useContext, useState, useRef} from 'react';
+
+// 1. Context 이름 변경하기, 작명규치 - view이름 + Context
+const NotificationViewContext = createContext();
+
+// 2. Hook 이름 변경하기, 작명 규칙- use + view이름 + State
+export const useNotificationViewState = () => {
+  const context = useContext(NotificationViewContext);
+  if (!context) {
+    throw new Error(
+      'useNotificationViewState must be used within a NotificationViewStateProvider',
+    );
+  }
+  return context;
+};
+//provider작명규칙 - view이름 + Provider
+export const NotificationViewStateProvider = ({children}) => {
+  // 3. 필요한 상태 추가하기
+  const [message, setMessage] = useState('');
+  const [DScode, setDScode] = useState(-1);
+  const [refreshing, setRefreshing] = useState(false);
+  const [notificationData, setNotificationData] = useState();
+  const [notificationsData, setNotificationsData] = useState([]);
+
+  const ref = {};
+
+  const state = {
+    message,
+    DScode,
+    refreshing,
+    notificationData,
+    notificationsData,
+  };
+
+  const actions = {
+    setMessage,
+    setDScode,
+    setRefreshing,
+    setNotificationData,
+    setNotificationsData,
+  };
+
+  return (
+    <NotificationViewContext.Provider value={{ref, state, actions}}>
+      {children}
+    </NotificationViewContext.Provider>
+  );
+};
