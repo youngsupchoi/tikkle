783a1ac78499deb4aac7dcaf7a5e3b905a59a781
sjsjmine129 <sjsjmine129@gmail.com>
2023-10-16 15:01:29 +0900

    TEST/android

--- android/app/src/main/java/com/tikkle_revive_ios/MainApplication.java
+++ android/app/src/main/java/com/tikkle_revive_ios/MainApplication.java
@@ -21,11 +21,11 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
+
+          return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            new ReactNativePushNotificationPackage() // <---- Add the Package
+        );
         }
 
         @Override
