adf64d2e913b580a9e91aa4a8fa5b4fcc4b10cc7
sjsjmine129 <sjsjmine129@gmail.com>
2023-11-28 18:17:34 +0900

    Fix/Gray logo

--- src/presentationLayer/view/components/globalComponents/Headers/HomeHeader.js
+++ src/presentationLayer/view/components/globalComponents/Headers/HomeHeader.js
@@ -55,7 +55,7 @@ export default function HomeHeader(props) {
         <UNIQUE27
           customStyle={{
             paddingLeft: 2,
-            color: COLOR_PRIMARY,
+            color: COLOR_GRAY,
           }}>
           TIKKLE
         </UNIQUE27>
